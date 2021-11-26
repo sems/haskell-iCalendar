016af7030e3e975900a9a105f3a1c9c9cac4eac3
Sem <me@sems.dev>
2021-11-26 16:43:23 +0100

    Ex. 1 and 2
    Co-authored-by: Amberbm <Amberbm@users.noreply.github.com>

--- src/DateTime.hs
+++ src/DateTime.hs
@@ -2,6 +2,8 @@ module DateTime where
 
 import ParseLib.Abstract
 import Prelude hiding ((<$), ($>), (<*), (*>), sequence)
+import Data.Char
+import Data.Maybe
 
 -- | "Target" datatype for the DateTime parser, i.e, the parser should produce elements of this type.
 data DateTime = DateTime { date :: Date
@@ -28,20 +30,56 @@ newtype Minute = Minute { runMinute :: Int } deriving (Eq, Ord)
 newtype Second = Second { runSecond :: Int } deriving (Eq, Ord)
 
 
--- Exercise 1
-parseDateTime :: Parser Char DateTime
-parseDateTime = undefined
+parseDate :: Parser Char Date
+parseDate = Date <$> (Year <$> parse4Digits)
+                 <*> (Month <$> parse2Digits)
+                 <*> (Day <$> parse2Digits)
 
+parseTime :: Parser Char Time
+parseTime = Time <$> (Hour <$> parse2Digits) 
+                 <*> (Minute <$> parse2Digits)  
+                 <*> (Second <$> parse2Digits) 
+
+-- From p0 - refresh
+parse2Digits :: Parser Char Int
+parse2Digits = (\x y -> 10 * x + y) <$> newdigit' <*> newdigit'
+
+-- adjusted from p0 --refresh
+parse4Digits :: Parser Char Int
+parse4Digits = (\w x y z -> 1000 * w + 100 * x + 10 * y + z) <$> newdigit' <*> newdigit' <*> newdigit' <*> newdigit'
+
+-- From the lecture notes
+newdigit' :: Parser Char Int 
+newdigit' = f <$> satisfy isDigit
+    where 
+        f c = ord c - ord '0'
+
+-- Exercise 1
+
+input :: String
+input = "19970610T172345Z"
+
+parseDateTime :: Parser Char DateTime
+parseDateTime = DateTime <$> parseDate 
+                         <* symbol 'T'
+                         <*> parseTime 
+                         <*> (True <$ symbol 'Z' <|> False <$ epsilon) -- Parser Char Bool
+
+-- parse parseDateTime input
 -- Exercise 2
 run :: Parser a b -> [a] -> Maybe b
-run = undefined
+run p input = getOutput (parse p input)
+    where 
+        getOutput [] = Nothing
+        getOutput ((x,[]):_) = Just x
+        getOutput (_:ys) = getOutput ys
 
 -- Exercise 3
 printDateTime :: DateTime -> String
 printDateTime = undefined
 
 -- Exercise 4
-parsePrint s = fmap printDateTime $ run parseDateTime s
+parsePrint s = fmap printDateTime $ run parseDateTime  s
 
 -- Exercise 5
 checkDateTime :: DateTime -> Bool
