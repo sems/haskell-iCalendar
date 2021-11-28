01cb3e527f0ff5f651b6cd5ca0eafc4fe0af2e4c
Sem <me@sems.dev>
2021-11-28 14:23:38 +0100

    Ex. 5

--- src/DateTime.hs
+++ src/DateTime.hs
@@ -78,17 +78,56 @@ run p input = getOutput (parse p input)
 printDateTime :: DateTime -> String
 printDateTime (DateTime date time bool) = printDate date ++ "T" ++printTime  time ++ printBool bool
     where
- 	    printDate (Date (Year y) (Month m) (Day d)) = show' y ++ show' m ++ show' d
-	    printTime (Time (Hour h) (Minute m) (Second s)) = show' h++ show' m ++ show' s
-	    printBool False = []
-	    printBool True = "Z"
+        printDate (Date (Year y) (Month m) (Day d)) = show' y ++ show' m ++ show' d
+        printTime (Time (Hour h) (Minute m) (Second s)) = show' h++ show' m ++ show' s
+        printBool False = []
+        printBool True = "Z"
         
 show' i | i < 10 = '0': show i -- alt version of show that keeps the string length consistant
         | otherwise = show i
 
 -- Exercise 4
-parsePrint s = fmap printDateTime $ run parseDateTime  s
+parsePrint s = fmap printDateTime $ run parseDateTime s
 
+
+-- data DateTime = DateTime { date :: Date
+--                          , time :: Time
+--                          , utc  :: Bool 
 -- Exercise 5
 checkDateTime :: DateTime -> Bool
-checkDateTime = undefined
+checkDateTime (DateTime d t _) = validDate d && validTime t
+
+-- data Date = Date { year  :: Year
+--                  , month :: Month
+--                  , day   :: Day }
+validDate :: Date -> Bool
+validDate dt@(Date y m _) = validYear y && validMonth m && validDay dt
+
+validTime :: Time -> Bool
+validTime (Time h m s) = validHour h && validMinute m && validSecond s
+
+isLeapYear :: Year -> Bool
+isLeapYear (Year y) | y `mod` 400 == 0 = True
+                    | y `mod` 100 == 0 = False
+                    | y `mod` 4   == 0 = True
+                    | otherwise        = False
+
+validDay :: Date -> Bool
+validDay (Date y (Month m) (Day d)) | m == 2 && isLeapYear y = 1 <= d && d <= 29 -- Leap year and february
+                                    | m == 2 = 1 <= d && d <= 28 -- february and no leap year
+                                    | m == 4 || m == 6 || m == 9 || m == 11 = 1 <= d && d <= 30 -- all months with a maximimum of 30 days, source: https://en.wikipedia.org/wiki/Gregorian_calendar
+                                    | otherwise = 1 <= d && d <= 31 -- all other with 31 days
+validYear :: Year -> Bool
+validYear (Year y) = 0 <= y
+
+validMonth :: Month -> Bool
+validMonth (Month m) = 1 <= m && m <= 23
+
+validHour :: Hour -> Bool
+validHour (Hour h) = 0 <= h && h <= 23
+
+validMinute :: Minute -> Bool
+validMinute (Minute m) = 0 <= m && m <= 59
+
+validSecond :: Second -> Bool
+validSecond (Second s) = 0 <= s && s <= 59
