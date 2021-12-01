d86a7209272151baf70be1269a2b991699b714b1
Sem <me@sems.dev>
2021-12-01 16:12:19 +0100

    refactor and clean code

--- src/Features.hs
+++ src/Features.hs
@@ -40,25 +40,26 @@ timeSpent sum (Calendar id v events) = diffInSeconds `div` 60
         filteredEvents = filter (filterBySummary sum) events -- list of events with given summary
 
 durationOfEvent :: Event -> Int
-durationOfEvent (Event _ _ (DateTime sDate@(Date (Year sYear) (Month sMonth) (Day sDay)) (Time (Hour sHour) (Minute sMinute) (Second sSecond)) _) (DateTime eDate@(Date (Year eYear) (Month eMonth) (Day eDay)) (Time (Hour eHour) (Minute eMinute) (Second eSecond)) _) _ _ _) = floor $ abs diffTime -- abs is needed because it is negative as default and floor to trim trailing zero's
+durationOfEvent (Event _ _ (DateTime sDate sTime _) (DateTime eDate eTime _) _ _ _) = floor $ abs diffTime -- abs is needed because it is negative as default and floor to trim trailing zero's
     where
         startTime :: DT.UTCTime
-        startTime = DT.UTCTime (toDay (toInteger sYear) sMonth sDay) $ DT.secondsToDiffTime $ toInteger $ timeInSecond sHour sMinute sSecond
+        startTime = DT.UTCTime (toDay sDate) (timeInDiffTime sTime)
         endTime :: DT.UTCTime
-        endTime = DT.UTCTime (toDay (toInteger eYear) eMonth eDay) $ DT.secondsToDiffTime $ toInteger $ timeInSecond eHour eMinute eSecond
+        endTime = DT.UTCTime (toDay eDate) (timeInDiffTime eTime) 
         diffTime :: Pico
         diffTime = DT.nominalDiffTimeToSeconds $ DT.diffUTCTime startTime endTime
 
--- hours -> minutes -> seconds
-timeInSecond :: Int -> Int -> Int -> Int
-timeInSecond h m s = (h * 3600) + (m * 60) + s
+timeInDiffTime :: Time -> DT.DiffTime
+timeInDiffTime (Time (Hour h) (Minute m) (Second s)) = DT.secondsToDiffTime seconds
+    where 
+        seconds :: Integer -- time in seconds after midnight.
+        seconds = toInteger $ (h * 3600) + (m * 60) + s
 
--- years -> month -> days
-toDay :: Integer -> Int -> Int -> DT.Day
-toDay = DT.fromGregorian
+toDay :: Date -> DT.Day
+toDay (Date (Year y) (Month m) (Day d)) = DT.fromGregorian (toInteger y) m d
 
 filterBySummary :: String -> Event -> Bool
-filterBySummary s (Event _ _ _ _ _ sum' _) = case sum' of
+filterBySummary s (Event _ _ _ _ _ sum' _) = case sum' of -- needed to use full paternmatch because 'sum' from prelude is also used.
     Nothing -> False
     Just x  -> x == s
 
