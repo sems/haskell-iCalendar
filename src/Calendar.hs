b98a25e07e7dcbf341a17c656ca87ee73409979b
Amberbm <56920715+Amberbm@users.noreply.github.com>
2021-12-01 07:33:29 +0100

    ex 7
    - might get refactured later (code is incompatible with  wrong input) but for now this works

--- src/Calendar.hs
+++ src/Calendar.hs
@@ -5,12 +5,13 @@ import Prelude hiding ((<$), ($>), (<*), (*>), sequence)
 import DateTime
 import System.IO -- for readCalendar
 import Data.Maybe
+import Data.List
 
 
 -- Exercise 6
 data Calendar = Calendar {
-    prodid :: String,
     version :: Version,
+    prodid :: String,
     events :: [Event]
 }
     deriving (Eq, Ord, Show)
@@ -49,29 +50,49 @@ data Token
   deriving (Eq, Ord, Show)
 
 scanCalendar :: Parser Char [Token]
-scanCalendar = many (choice [parseBegin, parseProId, parseVers, parseDtStamp, parseUid, parseStart, parseDtEnd, parseDesc, parseSum, parseLoc, parseEnd])
+scanCalendar = some (choice [parseBeginT, parseProIdT, parseVersT, parseDtStampT, parseUidT, parseStartT, parseDtEndT, parseDescT, parseSumT, parseLocT, parseEndT])
 
-
-parseBegin :: Parser Char Token
-parseBegin = pack (token "BEGIN:") ( BeginT <$ some (satisfy (/= '\r'))) (token "\r\n")
-parseProId = pack (token "PRODID:") ( ProdidT <$> some (satisfy (/= '\r'))) (token "\r\n")
-parseVers = pack (token "VERSION:") ( VersionT V2 <$ some (satisfy (/= '\r'))) (token "\r\n")
-parseDtStamp = pack (token "DTSTAMP:") ( getdt <$> some (satisfy (/= '\r'))) (token "\r\n")
+parseBeginT :: Parser Char Token
+parseBeginT = pack (token "BEGIN:") ( BeginT <$ some (satisfy (/= '\r'))) (token "\r\n")
+parseProIdT = pack (token "PRODID:") ( ProdidT <$> some (satisfy (/= '\r'))) (token "\r\n")
+parseVersT = pack (token "VERSION:") ( VersionT V2 <$ some (satisfy (/= '\r'))) (token "\r\n")
+parseDtStampT = pack (token "DTSTAMP:") ( getdt <$> some (satisfy (/= '\r'))) (token "\r\n")
     where getdt s = maybe Fail DtstampT (run parseDateTime s)
-parseUid =  pack (token "UID:") (UidT <$> some (satisfy (/= '\r'))) (token "\r\n")
-parseStart =  pack (token "DTSTART:") (getdt <$> some (satisfy (/= '\r'))) (token "\r\n")
+parseUidT =  pack (token "UID:") (UidT <$> some (satisfy (/= '\r'))) (token "\r\n")
+parseStartT =  pack (token "DTSTART:") (getdt <$> some (satisfy (/= '\r'))) (token "\r\n")
     where getdt s = maybe Fail StartT (run parseDateTime s)
-parseDtEnd  =  pack (token "DTEND:") (getdt <$> some (satisfy (/= '\r'))) (token "\r\n")
+parseDtEndT  =  pack (token "DTEND:") (getdt <$> some (satisfy (/= '\r'))) (token "\r\n")
     where getdt s = maybe Fail DtEndT (run parseDateTime s)
-parseDesc=  pack (token "DESCRIPTION:") (DescT <$> some (satisfy (/= '\r'))) (token "\r\n")
-parseSum =  pack (token "SUMMARY:") (SumT <$> some (satisfy (/= '\r'))) (token "\r\n")
-parseLoc =  pack (token "LOCATION:") (LocT <$> some (satisfy (/= '\r'))) (token "\r\n")
-parseEnd = pack (token "END:") ( EndT <$ some (satisfy (/= '\r'))) (token "\r\n")
+parseDescT=  pack (token "DESCRIPTION:") (DescT <$> some (satisfy (/= '\r'))) (token "\r\n")
+parseSumT =  pack (token "SUMMARY:") (SumT <$> some (satisfy (/= '\r'))) (token "\r\n")
+parseLocT =  pack (token "LOCATION:") (LocT <$> some (satisfy (/= '\r'))) (token "\r\n")
+parseEndT = pack (token "END:") ( EndT <$ some (satisfy (/= '\r'))) (token "\r\n")
 
-aa = run scanCalendar "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B2@example.com\r\nDTSTAMP:19970610T172345Z\r\nDTSTART:19970714T170000Z\r\nDTEND:19970715T040000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n"
+aa = recognizeCalendar "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B2@example.com\r\nDTSTAMP:19970610T172345Z\r\nDTSTART:19970714T170000Z\r\nDTEND:19970715T040000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n"
 
 parseCalendar :: Parser Token Calendar
-parseCalendar = undefined
+parseCalendar = pack (symbol BeginT) (Calendar <$> parseVers <*> parseProId <*> many parseEvent ) (symbol EndT)
+
+
+parseEvent = pack (symbol BeginT) ( getEvent <$> (sort <$> many (satisfy (/= EndT))) )(symbol EndT)
+ 
+
+--getEvent = Event <$> parseDtStamp <*> parseUid <*> parseStart <*> parsedtEnd <*> parseDesc <*> parseSum  <*> parseLoc
+
+
+getEvent (DtstampT a: UidT b: StartT c: DtEndT d: xs) = f $ getDesc xs
+            where getDesc [] = [Nothing,Nothing,Nothing]
+                  getDesc (DescT y:ys) = getSum [Just y] ys
+                  getDesc (_ :ys) = getSum [Nothing] ys
+                  getSum x [] = x ++ [Nothing,Nothing]
+                  getSum x (SumT y : ys) = getLoc (x ++ [Just y]) ys
+                  getSum x (_ :ys) = getLoc (x ++ [Nothing]) ys
+                  getLoc x [] = x ++ [Nothing]
+                  getLoc x [LocT y] = x ++ [Just y]
+                  f [x,y,z]= Event a b c d x y z
+
+parseVers = V2 <$ symbol (VersionT V2)
+parseProId = (\(ProdidT x) -> x) <$> anySymbol
 
 recognizeCalendar :: String -> Maybe Calendar
 recognizeCalendar s = run scanCalendar s >>= run parseCalendar
@@ -87,7 +108,7 @@ readCalendar fp = do
 -- Exercise 9
 -- DO NOT use a derived Show instance. Your printing style needs to be nicer than that :)
 printCalendar :: Calendar -> String
-printCalendar (Calendar p v evs) = 
+printCalendar (Calendar v p evs) = 
     "BEGIN:VCALENDAR\r\nVERSION:" ++ show v 
     ++ "\r\nPRODID:" ++ p ++ "\r\n" 
     ++ concatMap printEvent evs 
