f157ee4eea70052e0443a0f4a95eba2cbca10c56
Sem <me@sems.dev>
2021-12-01 13:25:40 +0100

    Added time library

--- src/Features.hs
+++ src/Features.hs
@@ -3,7 +3,7 @@ module Features where
 import DateTime
 import Calendar
 import Text.PrettyPrint.Boxes
-
+import Data.Time as DT
 
 -- Exercise 10
 -- prodid :: String,
@@ -30,7 +30,43 @@ checkOverlapping (Calendar id v events@(ev:evs)) = any p events || checkOverlapp
         between x y z | x < y = y < z | otherwise = False
 
 timeSpent :: String -> Calendar -> Int
-timeSpent = undefined
+timeSpent sum (Calendar id v events@(ev:evs)) = undefined -- filter (filterEvent sum) events -- list of events with given summary
+
+-- date :: Date
+-- , time :: Time
+-- , utc  :: Bool
+
+-- data Date = Date { year  :: Year
+--                  , month :: Month
+--                  , day   :: Day }
+
+-- newtype Hour   = Hour   { runHour   :: Int } deriving (Eq, Ord)
+-- newtype Minute = Minute { runMinute :: Int } deriving (Eq, Ord)
+-- newtype Second = Second { runSecond :: Int } deriving (Eq, Ord)
+durationOfEvent :: Event -> Integer
+durationOfEvent (Event _ _ (DateTime sDate@(Date (Year sYear) (Month sMonth) (Day sDay)) (Time (Hour sHour) (Minute sMinute) (Second sSecond)) _) (DateTime eDate@(Date (Year eYear) (Month eMonth) (Day eDay)) (Time (Hour eHour) (Minute eMinute) (Second eSecond)) _) _ _ _) = differenceInDays
+    where
+        differenceInDays = abs $ fromInteger $ DT.diffDays startDay endDay
+        --
+        startDay :: DT.Day
+        startDay = DT.fromGregorian (toInteger sYear) sMonth sDay
+        endDay :: DT.Day
+        endDay = DT.fromGregorian (toInteger eYear) eMonth eDay
+        startTimeSecond = (sHour * 3600) + (sMinute * 60) + sSecond
+        endTimeSecond = (eHour * 3600) + (eMinute * 60) + eSecond
+        ---
+        startTime :: DT.UTCTime
+        startTime = DT.UTCTime startDay $ DT.secondsToDiffTime startTimeSecond
+        endTime :: DT.UTCTime
+        endTime = DT.UTCTime endDay $ DT.secondsToDiffTime endTimeSecond
+        --
+        diffTime = DT.diffUTCTime startTime endTime
+
+
+filterBySummary :: String -> Event -> Bool
+filterBySummary s (Event _ _ _ _ _ sum' _) = case sum' of
+    Nothing -> False
+    Just x  -> x == s
 
 -- Exercise 11
 ppMonth :: Year -> Month -> Calendar -> String
